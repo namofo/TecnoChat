14b854f5c848b614d07fb27a848a180c15b5ef54
nadermgr@gmail.com <nadermgr@gmail.com>
2025-01-20 22:00:35 -0500

    solucionerrorvite

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,12 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  server: {
+    port: parseInt(process.env.PORT || '3000'),
+    host: true
+  },
+  preview: {
+    port: parseInt(process.env.PORT || '3000'),
+    host: true
+  }
 });
